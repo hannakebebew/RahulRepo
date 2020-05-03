5ca82bd7d752fdc67ec4c3e1d7f4bbad81d6d854
hanna <hannseyoum@gmail.com>
2020-05-02 21:19:59 -0400

    second commit

---
+++ src/test/java/addLogs/IntegrateTest/rahulgit.java
@@ -0,0 +1,11 @@
+package addLogs.IntegrateTest;
+
+public class rahulgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is a new class");
+	}
+
+}
