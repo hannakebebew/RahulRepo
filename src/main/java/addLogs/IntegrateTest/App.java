d8383a8df36230a8dd49b6f562c41c4d2748b2ce
hanna <hannseyoum@gmail.com>
2020-05-02 21:07:05 -0400

    Commit message

---
+++ src/main/java/addLogs/IntegrateTest/App.java
@@ -0,0 +1,13 @@
+package addLogs.IntegrateTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/addLogs/IntegrateTest/recordLogs.java
@@ -0,0 +1,20 @@
+package addLogs.IntegrateTest;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+public class recordLogs {
+	
+	// private static final Logger logger = LogManager.getLogger(recordLogs.class);  
+
+	@Test
+	public void useLogs() 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
